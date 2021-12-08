f9879bdcc8d670b5aad24f2b8555e67306bffe52
Pavan R. Mutalikdesai <60208890+pavandesai1989@users.noreply.github.com>
2021-12-08 19:56:20 +0530

    Update Largest Element.cpp
    return the index of the max element in the array

--- Arrays/Largest Element.cpp
+++ Arrays/Largest Element.cpp
@@ -1,1 +1,28 @@
 
+#include<iostream>
+using namespace std;
+
+/*
+    Return the index of the max element in the array
+*/
+int getmax(int arr[], int s)
+{
+	//assume first ele max
+	int res = 0;
+	for (int i = 1; i < s; i++)
+	{
+		if (arr[i] > arr[res])
+			res = i;
+	}
+	return res;
+}
+
+
+int main()
+{
+	int arr[] = {15,8,9,4,125,4,-1,7};
+	int size = sizeof(arr) / sizeof(int);
+
+	cout << "max ele index is "<<getmax(arr, size) <<" and ele is "<<arr[getmax(arr,size)]<< endl;
+	return 0;
+}
